eea440cb9cc2ab35469c64907612ceacb30fab17
Mohammad Kaif Shaikh <mohammadkaif6268@gmail.com>
2025-04-26 16:14:15 +0530

    Right Angled Triangle

--- Pattern Printing/RightAngledTrianglePattern.js
+++ Pattern Printing/RightAngledTrianglePattern.js
@@ -10,7 +10,28 @@ function rectangularStar(N){
       }
       console.log(str)
    }
+
+   
+for(let i = 1 ; i <= N ; i++){
+   let str = "" // use empty string instead of process
+   for(let j = 1 ; j <= i ; j++){
+   //  process.stdout.write("* ") //for same line
+   str+=j + " "
+
+
+   }
+   console.log(str)
 }
+}
+
+
+
+
+
+
+
+// number
+
 
 let N  = 5
 rectangularStar(N)
